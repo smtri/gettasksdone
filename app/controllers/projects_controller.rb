877f30aa2ddda511079f06c8c77e99d865b755c7
smtri <skip.a.bit@gmail.com>
2017-09-07 19:29:01 +0300

    edit projects, form in partial

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,7 +11,9 @@ class ProjectsController < ApplicationController
     @project = Project.new
   end
 
-  def edit; end
+  def edit
+    @project = Project.find(params[:id])
+  end
 
   def create
     @project = Project.new(project_params)
@@ -23,7 +25,15 @@ class ProjectsController < ApplicationController
     end
   end
 
-  def update; end
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
 
   def destroy; end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,3 @@
 Rails.application.routes.draw do
-  get 'projects/index'
-
-  get 'projects/show'
-
-  get 'projects/new'
-
-  get 'projects/edit'
-
-  get 'projects/create'
-
-  get 'projects/update'
-
-  get 'projects/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :projects
 end
